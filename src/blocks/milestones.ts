07c385a9619d2b5c180842f2838607d3b709e037
sohanvichare <svichare@stanford.edu>
2021-08-25 22:04:31 -0700

    jsdoc for html parsing in bfs

--- src/blocks/milestones.ts
+++ src/blocks/milestones.ts
@@ -1,16 +1,16 @@
 import {AUTODOC_IDENTIFIER} from '../main'
 import Confluence, {ConfluencePage} from '../utils/confluence'
+import {JSDOM} from 'jsdom'
 
 const MILESTONE_IDENTIFIER = 'milestones'
 
 const confluencePageContainsMilestone = (page: ConfluencePage): boolean => {
-  const parser = new DOMParser()
-  const html = parser.parseFromString(page.body, 'text/html')
+  const html = new JSDOM(page.body).window.document
   // BFS search text
   const queue: Node[] = [html.getRootNode()]
   while (queue.length > 0) {
     const node = queue.pop()
-    const value = node?.nodeValue
+    const value = node?.textContent
     if (
       value &&
       value
@@ -18,8 +18,18 @@ const confluencePageContainsMilestone = (page: ConfluencePage): boolean => {
         .includes(`${AUTODOC_IDENTIFIER} ${MILESTONE_IDENTIFIER}`.toLowerCase())
     )
       return true
+    if (!node) continue
+    if (node.hasChildNodes()) queue.push(...node.childNodes)
   }
   return false
+}
+
+const updateConfluenceMilestonePage = async (
+  page: ConfluencePage,
+  commitMessage: string,
+  confluenceGateway: Confluence
+): Promise<void> => {
+  const html = new JSDOM(page.body).window.document
 }
 
 const updateMilestonesOnConfluence = async (
@@ -32,7 +42,6 @@ const updateMilestonesOnConfluence = async (
   const milestonePages = confluencePages.filter(page =>
     confluencePageContainsMilestone(page)
   )
-  console.log(milestonePages)
 }
 
 export default updateMilestonesOnConfluence
